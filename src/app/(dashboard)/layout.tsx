d0ac7b0a08512138666c6c2c50bb91ab4fb60b95
Hacer Kılıç <hacerkilic194@gmail.com>
2024-12-09 20:00:02 +0300

    editing navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -18,8 +19,8 @@ export default function DashboardLayout({
         <Menu/>
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
-        R
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] ">
+        <Navbar/>
       </div>
     </div>
   );
