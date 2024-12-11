d68dd37b1701396f780d411f96f057daa4b5e618
Hacer Kılıç <hacerkilic194@gmail.com>
2024-12-11 20:52:08 +0300

    add attendancecharts

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
@@ -6,7 +7,7 @@ const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
-      <div className="w-full lg:w-2/3 ">
+      <div className="w-full lg:w-2/3 flex flex-col gap-2 ">
         {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
@@ -15,13 +16,16 @@ const AdminPage = () => {
           <UserCard type="staff" />
         </div>
         {/* MIDDLE CHARTS */}
-        <div className="flex gap-4 flex-col lg:flex:row"></div>
-        {/* COUNT CHARTS */}
-        <div className="w-full lg:w-1/3 h-[450px]">
-        <CountChart/>
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
         </div>
-        {/* ATTENDANCE CHARTS */}
-        <div className="w-full lg:w-2/3 h-[450px]"></div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3">R</div>
