a0d5b828133d62c1cb024f264541d2a1da2c77f0
Hacer Kılıç <hacerkilic194@gmail.com>
2024-12-11 15:27:19 +0300

    add user card

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,15 +12,19 @@ export default function DashboardLayout({
     <div className="h-screen flex">
       {/* {LEFT} */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
-        <Link href="/" className="flex items-center lg:justify-start justify-center  gap-2">
+        <Link
+          href="/"
+          className="flex items-center lg:justify-start justify-center  gap-2"
+        >
           <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">HACER'S SCHOOL</span>
+          <span className="hidden lg:block font-bold">HACER'S SCHOOL</span>
         </Link>
-        <Menu/>
+        <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] ">
-        <Navbar/>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col  ">
+        <Navbar />
+        {children}
       </div>
     </div>
   );
