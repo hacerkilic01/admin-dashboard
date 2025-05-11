91124c7b610aac06de9c3bbd96e4a1c85a8f91dd
Hacer Kılıç <hacerkilic194@gmail.com>
2025-05-11 21:45:57 +0300

    add role-based access control middleware using Clerk session metadata

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -2,8 +2,23 @@
 
 import * as Clerk from '@clerk/elements/common'
 import * as SignIn from '@clerk/elements/sign-in'
+import { useUser } from '@clerk/nextjs';
 import Image from 'next/image';
+import { useRouter } from "next/navigation";
+import { useEffect } from 'react';
+
 const LoginPage = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const role = user?.publicMetadata.role;
+
+    if (role) {
+      router.push(`/${role}`);
+    }
+  }, [user, router]);
 return (
     <div className="h-screen flex items-center justify-center bg-lamaSkyLight">
       <SignIn.Root>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
-import React from "react";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser();
   return (
     <div className="flex items-center justify-between p-4">
       {/* SEARCH BAR */}
-      <div className="hidden md:flex items-center rounded-full ring-[1.5px] ring-gray-300 gap-2 text-xs px-2">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="" width={14} height={14} />
         <input
           type="text"
@@ -15,24 +17,23 @@ const Navbar = () => {
       </div>
       {/* ICONS AND USER */}
       <div className="flex items-center gap-6 justify-end w-full">
-        <div className="flex items-center justify-center bg-white rounded-full w-7 h-7 cursor-pointer">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/message.png" alt="" width={20} height={20} />
         </div>
-        <div className="flex items-center justify-center bg-white rounded-full w-7 h-7 cursor-pointer relative ">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
           <Image src="/announcement.png" alt="" width={20} height={20} />
-          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white text-xs rounded-full"></div>
+          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">
+            1
+          </div>
         </div>
         <div className="flex flex-col">
-          <span className="text-xs leading-3 font-medium">Hacer Kılıç</span>
-          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+          <span className="text-xs leading-3 font-medium">John Doe</span>
+          <span className="text-[10px] text-gray-500 text-right">
+            {user?.publicMetadata?.role as string}
+          </span>
         </div>
-        <Image
-          src="/avatar.png"
-          alt=""
-          width={36}
-          height={36}
-          className="rounded-full"
-        />
+        {/* <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/> */}
+        <UserButton />
       </div>
     </div>
   );
