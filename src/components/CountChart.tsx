f5c36dfaa8bbea518e6f4c7d2dfa983fac62b175
Hacer Kılıç <hacerkilic194@gmail.com>
2025-05-21 20:45:16 +0300

    make student gender count chart dynamic with database data

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -7,55 +7,46 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Total",
-    count: 106,
-    fill: "white",
-  },
-  {
-    name: "Girls",
-    count: 53,
-    fill: "#FAE27C",
-  },
-  {
-    name: "Boys",
-    count: 53,
-    fill: "#C3EBFA",
-  },
-];
 
-const CountChart = () => {
+const CountChart = ({ boys, girls }: { boys: number; girls: number }) => {
+  const data = [
+    {
+      name: "Total",
+      count: boys+girls,
+      fill: "white",
+    },
+    {
+      name: "Girls",
+      count: girls,
+      fill: "#FAE27C",
+    },
+    {
+      name: "Boys",
+      count: boys,
+      fill: "#C3EBFA",
+    },
+  ];
   return (
-    <div className="bg-white rounded-xl w-full h-full p-4">
-      {/* TITLE */}
-      <div className="flex justify-between items-center">
-        <h1 className="text-lg font-semibold">Students</h1>
-        <Image src="/moreDark.png" alt="" width={20} height={20} />
-      </div>
-      {/* CHART */}
-      <div className="relative w-full h-[75%]">
-        <ResponsiveContainer>
-          <RadialBarChart
-            cx="50%"
-            cy="50%"
-            innerRadius="40%"
-            outerRadius="100%"
-            barSize={32}
-            data={data}
-          >
-            <RadialBar background dataKey="count" />
-          </RadialBarChart>
-        </ResponsiveContainer>
-        <Image
-          src="/maleFemale.png"
-          alt=""
-          width={50}
-          height={50}
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-        />
-      </div>
-      {/* BOTTOM */}
+    <div className="relative w-full h-[75%]">
+      <ResponsiveContainer>
+        <RadialBarChart
+          cx="50%"
+          cy="50%"
+          innerRadius="40%"
+          outerRadius="100%"
+          barSize={32}
+          data={data}
+        >
+          <RadialBar background dataKey="count" />
+        </RadialBarChart>
+      </ResponsiveContainer>
+      <Image
+        src="/maleFemale.png"
+        alt=""
+        width={50}
+        height={50}
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
     </div>
   );
 };
