7c6fa594fb274c2635b401f10bf1d23d2291ddd5
Hacer Kılıç <hacerkilic194@gmail.com>
2025-06-16 09:03:48 +0300

    edit announcement

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,5 +1,3 @@
-export const runtime = "nodejs"
-export const dynamic = "force-dynamic"
 import FormContainer from "@/components/FormContainer";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
